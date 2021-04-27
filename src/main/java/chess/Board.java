1a793f47a9b226f10e8df48889902c34ef251c51
CJSantee <santeedev@gmail.com>
2021-04-26 22:28:17 -0500

    JUnits for Board.java

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -2,33 +2,28 @@ package chess;
 import chess.Pieces.*;
 
 public class Board {
-    Spot[][] boxes;
+    private Spot[][] boxes;
 
     public static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
 
 
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
 
     public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
-    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
     public Board(){
         boxes = new Spot[8][8];
         this.resetBoard();
+    }
+
+    public Spot getBox(int x, int y) throws Exception{
+        if(x < 0 || x > 7 || y < 0 || y > 7){
+            throw new Exception("Index out of bound");
+        }
+        return boxes[x][y];
     }
 
     public void resetBoard(){
---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,74 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import chess.Board;
+import chess.Spot;
+import chess.Pieces.Bishop;
+import chess.Pieces.King;
+import chess.Pieces.Knight;
+import chess.Pieces.Pawn;
+import chess.Pieces.Queen;
+import chess.Pieces.Rook;
+
+public class BoardTest {
+    
+    private Board board;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        board = new Board();
+    }
+
+    @Test
+    void testGetBox() throws Exception{
+        Throwable exception = assertThrows(Exception.class, () -> board.getBox(-1, -1));
+        assertThrows(Exception.class, () -> board.getBox(8, 4), "x value out of bound should throw exception");
+        assertThrows(Exception.class, () -> board.getBox(4, 8), "y value out of bound should throw exception");
+        assertEquals("Index out of bound", exception.getMessage(), "should display \"Index out of bound\" message");
+        assertTrue(board.getBox(0, 0) instanceof Spot, "should return Spot object");
+    }
+
+    @Test
+    void testResetBoard() throws Exception{
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                assertNotNull(board.getBox(i, j), "Index (" + i + ", " + j + ") should not be null");
+            }
+        }
+        assertTrue(board.getBox(0, 0).getPiece() instanceof Rook, "(0, 0) should be a Rook");
+        assertTrue(board.getBox(0, 1).getPiece() instanceof Knight, "(0, 1) should be a Knight");
+        assertTrue(board.getBox(0,2).getPiece() instanceof Bishop, "(0, 2) should be a Bishop");
+        assertTrue(board.getBox(0,3).getPiece() instanceof King, "(0, 3) should be a King");
+        assertTrue(board.getBox(0,4).getPiece() instanceof Queen, "(0, 4) should be a Queen");
+        assertTrue(board.getBox(0,5).getPiece() instanceof Bishop, "(0, 5) should be a Bishop");
+        assertTrue(board.getBox(0,6).getPiece() instanceof Knight, "(0, 6) should be a Knight");
+        assertTrue(board.getBox(0,7).getPiece() instanceof Rook, "(0, 7) should be a Rook");
+
+        for(int i = 0; i < 8; i++){
+            assertTrue(board.getBox(1, i).getPiece() instanceof Pawn, "(1, "+i+") should be a Pawn");
+        }
+        for(int i = 0; i < 8; i++){
+            assertTrue(board.getBox(6, i).getPiece() instanceof Pawn, "(6, "+i+") should be a Pawn");
+        }
+        assertTrue(board.getBox(7, 0).getPiece() instanceof Rook, "(7, 0) should be a Rook");
+        assertTrue(board.getBox(7, 1).getPiece() instanceof Knight, "(7, 1) should be a Knight");
+        assertTrue(board.getBox(7,2).getPiece() instanceof Bishop, "(7, 2) should be a Bishop");
+        assertTrue(board.getBox(7,3).getPiece() instanceof King, "(7, 3) should be a King");
+        assertTrue(board.getBox(7,4).getPiece() instanceof Queen, "(7, 4) should be a Queen");
+        assertTrue(board.getBox(7,5).getPiece() instanceof Bishop, "(7, 5) should be a Bishop");
+        assertTrue(board.getBox(7,6).getPiece() instanceof Knight, "(7, 6) should be a Knight");
+        assertTrue(board.getBox(7,7).getPiece() instanceof Rook, "(7, 7) should be a Rook");
+
+        for(int r = 2; r < 6; r++){
+            for(int c = 0; c < 8; c++){
+                assertNull(board.getBox(r, c).getPiece(), "("+r+", "+c+") should not contain a piece");
+            }
+        }
+    }
+}
