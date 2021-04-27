31dec16b88f5853725f9957074f10260b076e959
CJSantee <santeedev@gmail.com>
2021-04-27 08:47:05 -0500

    Added JUnits for Board

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import chess.Board;
@@ -20,29 +22,33 @@ public class BoardTest {
     
     private Board board;
 
-    @BeforeEach
-    void setUp() throws Exception{
-        board = new Board();
-    }
-
     @Test
-    void testBoard() throws Exception{
+    void testBoardException() throws Exception{
         Throwable exception = assertThrows(Exception.class, () -> { board = new Board(" "); });
         assertEquals("Invalid input String", exception.getMessage());
     }
 
     @Test
-    void testGetBox() throws Exception{
-        Throwable exception = assertThrows(Exception.class, () -> board.getBox(-1, -1));
-        assertThrows(Exception.class, () -> board.getBox(8, 4), "x value out of bound should throw exception");
-        assertThrows(Exception.class, () -> board.getBox(4, 8), "y value out of bound should throw exception");
-        assertEquals("Index out of bound", exception.getMessage(), "should display \"Index out of bound\" message");
-        assertTrue(board.getBox(0, 0) instanceof Spot, "should return Spot object");
+    void testFENConstructor() throws Exception{
+        board = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        testStandardSetup();
+    }
+
+    @Test
+    void testDefaultConstructor() throws Exception{
+        board = new Board();
+        testStandardSetup();
     }
 
     @Test
     void testResetBoard() throws Exception{
+        board = new Board("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1");
         board.resetBoard();
+        testStandardSetup();
+    }
+
+    @Test
+    void testStandardSetup() throws Exception{
         for(int i = 0; i < 8; i++){
             for(int j = 0; j < 8; j++){
                 assertNotNull(board.getBox(i, j), "Index (" + i + ", " + j + ") should not be null");
@@ -78,9 +84,32 @@ public class BoardTest {
             }
         }
     }
+    
+    @Nested
+    @DisplayName("Functions with Standard Setup")
+    class BoardFunctions {
 
-    @Test
-    void testExport(){
-        assertEquals("rnbqkbnrpppppppp                                PPPPPPPPRNBQKBNR", board.export(), "Should export standard setup");
+        @BeforeEach
+        void setUp(){
+            board = new Board();
+        }
+
+        @Test
+        void testGetBox() throws Exception{
+            Throwable exception = assertThrows(Exception.class, () -> board.getBox(-1, -1));
+            assertThrows(Exception.class, () -> board.getBox(8, 4), "x value out of bound should throw exception");
+            assertThrows(Exception.class, () -> board.getBox(4, 8), "y value out of bound should throw exception");
+            assertEquals("Index out of bound", exception.getMessage(), "should display \"Index out of bound\" message");
+            assertTrue(board.getBox(0, 0) instanceof Spot, "should return Spot object");
+        }
+
+        @Test
+        void testFen(){
+            assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", board.fen(), "Should export standard setup in FEN");
+        }
+
     }
+
+    
+
 }
